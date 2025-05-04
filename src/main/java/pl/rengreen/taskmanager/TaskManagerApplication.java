a7c5352dd37d703c54eb01c5770fea421366468c
Kiran <kirankifan7348@gmail.com>
2025-05-04 14:56:21 +0530

    made one change

--- src/main/java/pl/rengreen/taskmanager/TaskManagerApplication.java
+++ src/main/java/pl/rengreen/taskmanager/TaskManagerApplication.java
@@ -3,6 +3,7 @@ package pl.rengreen.taskmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//made a change
 @SpringBootApplication
 public class TaskManagerApplication {
 
